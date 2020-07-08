4a013a0dc8b90bba3a117b1dcc0f4ed26327d1de
Violeta <violetalopesalves@gmail.com>
2020-07-08 13:33:57 +0100

    debug

--- face.js
+++ face.js
@@ -2,9 +2,13 @@ const video = document.querySelector('.webcam');
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
 const faceCanvas = document.querySelector('.face');
-const faceCtx = canvas.getContext('2d');
-
+const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
+// const optionsInputs = document.querySelectorAll('.controls input[type="range"]');
+
+// console.log(optionsInputs);
+
+
 console.log(video, canvas, faceCanvas, faceDetector);
 
 function populateVideo(){
