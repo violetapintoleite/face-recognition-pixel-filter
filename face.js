37e61fe7b70fff5ac369e81d6b69cf8c53c8a117
Violeta <violetalopesalves@gmail.com>
2020-07-08 14:23:59 +0100

    Drawing the filter
    Pulling the face image, changing scales and putting back on pixelized

--- face.js
+++ face.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext('2d');
 const faceCanvas = document.querySelector('.face');
 const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
+const SIZE = 10;
+const SCALE = 1.5;
 // const optionsInputs = document.querySelectorAll('.controls input[type="range"]');
 
 async function populateVideo() {
@@ -22,6 +24,7 @@ async function populateVideo() {
 async function detect(){
    const faces = await faceDetector.detect(video);
    faces.forEach(drawFace);
+    faces.forEach(censor);
    requestAnimationFrame(detect);
 }
 function drawFace(face) {
@@ -31,7 +34,41 @@ function drawFace(face) {
     ctx.lineWidth = 2;
     ctx.strokeRect(left, top, width, height);
 }
+function censor({boundingBox: face}){
+    faceCtx.imageSmoothingEnabled = false;
+    faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+    // draw the small face
+faceCtx.drawImage(
+    // 5 source args
+    video, // where the source comes from
+    face.x, // where do we start pull from
+    face.y,
+    face.width,
+    face.height,
+    // 4 draw args
+    face.x,
+    face.y,
+    SIZE,
+    SIZE,
+);
+// draw the small face back on, but scale up
 
+const width = face.width * SCALE;
+const height = face.height * SCALE;
+faceCtx.drawImage(
+    faceCanvas, // source
+    face.x,
+    face.y,
+    SIZE,
+    SIZE,
+
+    // Drawing args
+    face.x,
+    face.y,
+    face.width,
+    face.height,
+)
+}
 
 populateVideo().then(detect);
 
