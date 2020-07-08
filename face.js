e9470a1beba6069c8a0249213fc583727bc79b72
Violeta <violetalopesalves@gmail.com>
2020-07-08 13:10:32 +0100

    selected elements on the page to start working them

--- face.js
+++ face.js
@@ -1,1 +1,16 @@
-console.log('its working!');
+const video = document.querySelector('.webcam');
+const canvas = document.querySelector('.video');
+const ctx = canvas.getContext('2d');
+const faceCanvas = document.querySelector('.face');
+const faceCtx = canvas.getContext('2d');
+
+const faceDetector = new window.FaceDetector();
+console.log(video, canvas, faceCanvas, faceDetector);
+
+function populateVideo(){
+    const stream = navigator.mediaDevices.getUserMedia({
+        video: { width: 1280, height: 720 }
+    });
+    console.log(stream);
+}
+
