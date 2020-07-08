2d5b1be68399052cb80b7434d680b65bcd511b56
Violeta <violetalopesalves@gmail.com>
2020-07-08 14:00:20 +0100

    Grabed video from camera, detected face and draw the face limits

--- face.js
+++ face.js
@@ -6,15 +6,32 @@ const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
 // const optionsInputs = document.querySelectorAll('.controls input[type="range"]');
 
-// console.log(optionsInputs);
-
-
-console.log(video, canvas, faceCanvas, faceDetector);
-
-function populateVideo(){
-    const stream = navigator.mediaDevices.getUserMedia({
-        video: { width: 1280, height: 720 }
+async function populateVideo() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 640, height: 360 }
     });
-    console.log(stream);
+    video.srcObject = stream;
+    await video.play();
+    console.log(video.videoWidth, video.videoHeight);
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    faceCanvas.width = video.videoWidth;
+    faceCanvas.height = video.videoHeight;
 }
+
+async function detect(){
+   const faces = await faceDetector.detect(video);
+   faces.forEach(drawFace);
+   requestAnimationFrame(detect);
+}
+function drawFace(face) {
+    const { width, height, top, left } = face.boundingBox;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = '#ffc600';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(left, top, width, height);
+}
+
+
+populateVideo().then(detect);
 
